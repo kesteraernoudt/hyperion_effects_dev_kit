4f9a00cb243e5e381ba2764d4854177c0fc393cb
Kester Aernoudt <kesteraernoudt@yahoo.com>
2018-12-24 14:22:52 +0100

    using json file for effects configuration just like hyperion server

--- main.py
+++ main.py
@@ -13,38 +13,46 @@ import gui
 import hyperion
 import runpy
 import json_client
-
+import sys
 
 # Change this to your Pis / Hyperion data. There is no harm done if wrong data is set here.
 # The connection will simply time out.
-hyperion_host = '192.168.178.32'
+hyperion_host = '192.168.1.177'
 hyperion_port = 19444
 
 # Change this according to your led configuration.
-horizontal_led_num = 17
-vertical_led_num = 9
-first_led_offset_num = 17
+horizontal_led_num = 30
+vertical_led_num = 30
+first_led_offset_num = 0
 leds_in_clockwise_direction = False
 has_corner_leds = False
 
 
-def run_effect():
+def run_effect(my_effect):
     """
     Runs the module effect. Copy any hyperion effect code in this module or create your own.
     Note that effects that call hyperion.setColor(r, g, b) or hyperion.setImage(img) are not supported.
     """
-    runpy.run_module("effect")
+    import json
+    import sys
+    with open('effects/' + my_effect + '.json', 'r') as f:
+        effects_data = json.load(f)
+    
+    hyperion.args = effects_data['args']
+    print "Running effect: " + effects_data['name']
+    #runpy.run_module('effect')
+    runpy.run_path('effects/' + effects_data['script'])
 
 
-def main():
+def main(my_effect):
     hyperion.init(horizontal_led_num, vertical_led_num, first_led_offset_num, leds_in_clockwise_direction,
                   has_corner_leds)
 
     # Open the connection to the json server. Uncomment if you do not want to send data to the server.
-    json_client.open_connection(hyperion_host, hyperion_port)
+    #json_client.open_connection(hyperion_host, hyperion_port)
 
     # create own thread for the effect
-    effect_thread = Thread(target=run_effect)
+    effect_thread = Thread(target=run_effect, args=[my_effect])
 
     effect_thread.start()
     gui.createWindow()
@@ -55,10 +63,13 @@ def main():
     effect_thread.join()
 
     # close potential connections
-    json_client.close_connection()
+    #json_client.close_connection()
     print("Exiting")
 
 
 if __name__ == '__main__':
-    main()
+    my_effect = 'effect'
+    if len(sys.argv) > 1:
+        my_effect = sys.argv[1]
+    main(my_effect)
 
